a25a730127bbb04dfb7c7fd6601118a63e0745db
Emerson Bilieri Claudelino <ebilieri@gmail.com>
2020-02-01 23:56:31 -0300

    config injeção de dependencia

--- VexTel.App/Startup.cs
+++ VexTel.App/Startup.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using VexTel.Domain.Contracts.IRepositories;
+using VexTel.Domain.Contracts.IServices;
+using VexTel.Repository.Context;
+using VexTel.Repository.Repositories;
+using VexTel.Services;
 
 namespace VexTel.App
 {
@@ -20,12 +28,39 @@ namespace VexTel.App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // String de conexão com o Banco de dados (MySql)
+            //var connectionString = Configuration.GetConnectionString("VexTelConnection");
+            var connectionString = Configuration.GetConnectionString("VexTelRemoteMysql");
+
+            // Configurar context banco de dados
+            services.AddDbContext<VexTelContext>(option =>
+                    option.UseLazyLoadingProxies()
+                        .UseMySql(connectionString, m =>
+                            m.MigrationsAssembly("VexTel.Repository")));
+
+
+
+            // Mapeamento Injeção de dependencia
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<IDDDRepository, DDDRepository>();
+            services.AddScoped<IPlanoRepository, PlanoRepository>();
+            services.AddScoped<ICustoChamadaRepository, CustoChamadaRepository>();
+
+            services.AddScoped<IDDDService, DDDService>();
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddMvc().AddNewtonsoftJson(options => { options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
