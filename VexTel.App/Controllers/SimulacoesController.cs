e9795c07b36a08e37bbef35b1664f2d5de5ecd30
Emerson Bilieri Claudelino <ebilieri@gmail.com>
2020-02-02 22:51:24 -0300

    simulação controller

--- VexTel.App/Controllers/SimulacoesController.cs
+++ VexTel.App/Controllers/SimulacoesController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VexTel.Domain.Contracts.IServices;
+using VexTel.Domain.Entities;
 
 namespace VexTel.App.Controllers
 {
@@ -16,6 +17,21 @@ namespace VexTel.App.Controllers
         public SimulacoesController(ISimulacaoChamadaService simulacaoChamadaService)
         {
             _simulacaoChamadaService = simulacaoChamadaService;
+        }
+
+        [HttpPost]
+        public ActionResult Simular(SimulacaoChamada simulacaoChamada)
+        {
+            try
+            {
+                //simulacaoChamada.
+                _simulacaoChamadaService.Simular(simulacaoChamada);
+                return Ok(simulacaoChamada);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
         }
     }
 }
