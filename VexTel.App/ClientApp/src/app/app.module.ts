1eb2fb064fb2cca7de04f4e7b3398f18121ac246
Emerson Claudelino <tc.claudelino@kantaribopemedia.com>
2020-02-03 17:24:36 -0300

    config locale

--- VexTel.App/ClientApp/src/app/app.module.ts
+++ VexTel.App/ClientApp/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt, 'pt');
+
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -29,6 +36,7 @@ import { PlanoService } from './services/planoService';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
@@ -36,6 +44,7 @@ import { PlanoService } from './services/planoService';
     ])
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
     DDDService,
     PlanoService,
     SimulacaoChamadaService
